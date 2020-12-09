6e9d5dc90d1c2d6e26848119583e57967b91d196
chestercolita <chester@nabepero.co.jp>
2020-12-09 13:34:36 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Models\User;
+use App\Models\Role;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/create', function() {
+    $user = User::findOrFail(1);
+    $user->roles()->create([
+        'name' => 'Author'
+    ]);
+});
+
+Route::get('/read', function() {
+    $user = User::findOrFail(1);
+    foreach ($user->roles as $role) {
+        echo $role->name;
+    }
+});
+
+Route::get('/update', function() {
+   $user = User::findOrFail(1);
+   if($user->has('roles')) {
+       foreach($user->roles as $role) {
+           if($role->name == 'Administrator') {
+               $role->update(['name' => 'administrator']);
+           }
+       }
+   }
+});
+
+Route::get('/delete', function() {
+    $user = User::findOrFail(1);
+    foreach($user->roles as $role) {
+        $role->whereId(3)->delete();
+    }
+});
+
+Route::get('/attach', function() {
+    $user = User::findOrFail(1);
+    $user->roles()->attach(5);
+});
+
+Route::get('/detach', function() {
+    $user = User::findOrFail(1);
+    $user->roles()->detach(5);
+});
+
+Route::get('/sync', function() {
+    $user = User::findOrFail(1);
+    $user->roles()->sync([4]);
+});
